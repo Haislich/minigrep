1615e8eec5697181dba6ac4e811c4dc88467820a
Puddish <josedlv936@gmail.com>
2022-10-16 19:09:17 +0200

    added Readme.MD and added explanations to the code

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub struct Config{
 }
 impl Config{
     pub fn new(args: &[String]) -> Result<Config,&str>{
+        // Constructor for Config struct, it returns a Result containing
+        // an initialized Config or a an error explaining why it couldn't be initialized
         match args.len(){
             3 => (),
             _ => return Err("Usage: minigrep pattern file")
@@ -18,29 +20,37 @@ impl Config{
         Ok(Config {pattern,filename,case_sensitive})
     } 
 }
+// The dyn Error is used because we're propagating the error from the read_to_string
+// the problem is that we don't know at compile time what error might be.
+// For example it could be that the file cannot be read, the file doesn't exists or the
+// characters are not all utf-8 vlaid characters
 pub fn run(config:Config)-> Result<(),Box<dyn Error>>{
+    // Function that does that prints if there are matches.
+
+    // The ? operator returns the error if encountered, else continues 
     let contents = fs::read_to_string(config.filename)?;
     let results  = match config.case_sensitive{
-        true=>search(&config.pattern, &contents),
-        false=>search_case_insensitive(&config.pattern, &contents)
+        true  => search(&config.pattern, &contents),
+        false => search_case_insensitive(&config.pattern, &contents)
     };
     for line in results{
         println!("{line}")
     }
     Ok(())
 }
-pub fn search<'a>(pattern: &str, contents:&'a str)-> Vec<&'a str>{
+fn search<'a>(pattern: &str, contents:&'a str)-> Vec<&'a str>{
     let mut results = Vec::new();
-    for line in contents.lines(){
+    // More functional approach
+    contents.lines().for_each(|line| {
         if line.contains(pattern){
-            results.push(line);
-        };
-    };
+            results.push(line)}
+        });
     results
 }
-pub fn search_case_insensitive<'a>(pattern: &str, contents:&'a str) -> Vec<&'a str>{
+fn search_case_insensitive<'a>(pattern: &str, contents:&'a str) -> Vec<&'a str>{
     let pattern = pattern.to_lowercase();
     let mut results: Vec<&str> = Vec::new();
+    // More 'standard' approach
     for line in contents.lines(){
         if line.to_lowercase().contains(&pattern){
             results.push(line);
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use minigrep::{Config,run};
 
 
 fn main() {
+    // Get arguments from input and store them as a vector of string.
+    // after using collect Rust has no way of knowing wich type of vector 
+    // we want so we need to help it, assigning manually the value.
     let args:Vec<String> = env::args().collect();
+
+    // Te constructor for the Config struct returns a Result.
+    // Using the unwrap or else we are saying unwrap (extract the value from Ok),
+    // if the value is not ok then perform some operation on the error.
     let config = Config::new(&args).unwrap_or_else(|err| {
+        // eprintln!() is a macro that redirects its output to the standard error
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
